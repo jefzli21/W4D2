9a4308b2a1233de6d8ac1fefc0d8d7709a0cf6cb
AA Student <appacademy@Mini-7.local>
2022-07-06 12:20:14 -0700

    sliding

--- chess/pieces/sliding.rb
+++ chess/pieces/sliding.rb
@@ -1,8 +1,32 @@
 
 
 module Slideable
+
     def moves
+        newarr = []
+        pos = self.pos
+        self.move_dirs.each do |pair|
+            newpos = [(pos[0] + pair[0]), (pos[1] + pair[1])]
+            if @board[newpos].nil?
+                self.recursion(newpos, pair)
+         
+
+
+            
     end
+
+    def recursion(pos, dir)
+        newarr = [pos]
+
+        newpos = [(pos[0] + dir[0]), (pos[1] + dir[1])]
+
+            newarr << newpos
+        elsif 
+
+
+        
+    end
+
 
 
 end
